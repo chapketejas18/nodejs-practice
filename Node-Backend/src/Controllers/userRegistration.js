24a86cc81c053f4a79e80be33f12a7262c9354b9
tejaschapke18 <tejas.chapke@successive.tech>
2024-07-09 20:17:08 +0530

    fix

--- Node-Backend/src/Controllers/userRegistration.js
+++
@@ -1,6 +0,0 @@
-exports.registerUser = (req, res) => {
-  const { username, email } = req.body;
-  res
-    .status(200)
-    .json({ message: `User ${username} registered with email ${email}` });
-};
